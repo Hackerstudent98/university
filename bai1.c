558072cf35ac7359a4bace57d85b48e65f4d8236
Hackerstudent98 <lacaotungchao@gmail.com>
2018-01-26 15:16:36 +0700

    first upload
    2 function , array_rand(), array_inp()

---
+++ bai1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void array_rand(int *list, int count)
+{
+	int i = 0;
+	srand( time(NULL) );
+	while(--count > 0)
+ 	{
+	 	*(list + i) = rand() % 101; 
+	 	i++;
+    }
+
+}
+void array_inp(int *list, int count)
+{
+	int i = 0;
+	
+	while(--count > 0)
+	{
+		printf("%d ", *(list+i));
+		i++;	
+	}
+	
+}
+int main(int argc, char *argv[])
+{
+	int mang[15]={};
+	array_rand(mang,16);
+	array_inp(mang,16);
+	getchar();
+	return 0;
+}
